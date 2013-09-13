b92500bb5251070f27dd4bbc4ad78a4b39333d4a
Mike Slinn <mslinn@micronauticsresearch.com>
2013-09-13 07:31:37 -0700

    -

--- assignment/src/main/scala/ConfigDemo.scala
+++ assignment/src/main/scala/ConfigDemo.scala
@@ -1,5 +1,6 @@
 import com.typesafe.config.{Config, ConfigFactory}
 import collection.JavaConverters._
+import java.lang
 
 object ConfigDemo extends App {
   implicit class ConfigHelper(config: Config) {
@@ -8,17 +9,32 @@ object ConfigDemo extends App {
     def getBoolean(key: String, defaultValue: Boolean): Boolean =
       if (keys contains key) config.getBoolean(key) else defaultValue
 
+    def getBooleanList(key: String, defaultValue: List[Boolean]): List[Boolean] =
+      if (keys contains key) config.getBooleanList(key).asScala.map(_.booleanValue).toList else defaultValue
+
     def getDouble(key: String, defaultValue: Double): Double =
       if (keys contains key) config.getDouble(key) else defaultValue
+
+    def getDoubleList(key: String, defaultValue: List[Double]): List[Double] =
+      if (keys contains key) config.getDoubleList(key).asScala.map(_.doubleValue).toList else defaultValue
 
     def getInt(key: String, defaultValue: Int): Int =
       if (keys contains key) config.getInt(key) else defaultValue
 
+    def getIntList(key: String, defaultValue: List[Int]): List[Int] =
+      if (keys contains key) config.getIntList(key).asScala.map(_.intValue).toList else defaultValue
+
     def getLong(key: String, defaultValue: Long): Long =
       if (keys contains key) config.getLong(key) else defaultValue
 
+    def getLongList(key: String, defaultValue: List[Long]): List[Long] =
+      if (keys contains key) config.getLongList(key).asScala.map(_.longValue).toList else defaultValue
+
     def getString(key: String, defaultValue: String): String =
       if (keys contains key) config.getString(key) else defaultValue
+
+    def getStringList(key: String, defaultValue: List[String]): List[String] =
+      if (keys contains key) config.getStringList(key).asScala.toList else defaultValue
   }
 
   implicit class ConfigHelper2(string: String) {
