796fc23527ee81540f658a55dfa910ad7a8026c9
Mike Slinn <mslinn@micronauticsresearch.com>
2013-09-13 07:23:25 -0700

    -

--- assignment/src/main/scala/ConfigDemo.scala
+++ assignment/src/main/scala/ConfigDemo.scala
@@ -4,6 +4,12 @@ import collection.JavaConverters._
 object ConfigDemo extends App {
   implicit class ConfigHelper(config: Config) {
     def keys: Set[String] = config.entrySet.asScala.map(_.getKey).toSet
+
+    def getInt(key: String, defaultValue: Int): Int =
+      if (keys contains key) config.getInt(key) else defaultValue
+
+    def getString(key: String, defaultValue: String): String =
+      if (keys contains key) config.getString(key) else defaultValue
   }
 
   implicit class ConfigHelper2(string: String) {
@@ -18,4 +24,6 @@ object ConfigDemo extends App {
                          |array : [ 1, 2, 3]
                          |""".stripMargin.toConf
   println(s"Keys: ${config.keys}")
+  println(s"""getString("keyName1"): ${config.getString("keyName1", "defaultValue")}""")
+  println(s"""getString("nonExistantKey"): ${config.getString("nonExistantKey", "defaultValue")}""")
 }
