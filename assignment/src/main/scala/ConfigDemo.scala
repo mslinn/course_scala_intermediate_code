fc5812bcf8fac95dfe53c10c83722aa3042253bf
Mike Slinn <mslinn@micronauticsresearch.com>
2013-09-13 07:25:36 -0700

    -

--- assignment/src/main/scala/ConfigDemo.scala
+++ assignment/src/main/scala/ConfigDemo.scala
@@ -5,8 +5,17 @@ object ConfigDemo extends App {
   implicit class ConfigHelper(config: Config) {
     def keys: Set[String] = config.entrySet.asScala.map(_.getKey).toSet
 
+    def getBoolean(key: String, defaultValue: Boolean): Boolean =
+      if (keys contains key) config.getBoolean(key) else defaultValue
+
+    def getDouble(key: String, defaultValue: Double): Double =
+      if (keys contains key) config.getDouble(key) else defaultValue
+
     def getInt(key: String, defaultValue: Int): Int =
       if (keys contains key) config.getInt(key) else defaultValue
+
+    def getLong(key: String, defaultValue: Long): Long =
+      if (keys contains key) config.getLong(key) else defaultValue
 
     def getString(key: String, defaultValue: String): String =
       if (keys contains key) config.getString(key) else defaultValue
