acce4206449bc4870de40887e57e403a63af982f
Mike Slinn <mslinn@micronauticsresearch.com>
2013-10-10 15:02:20 -0700

    Added example of server-sent events

---
+++ play2.1_SSE/project/Build.scala
@@ -0,0 +1,17 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "WikiGrowth"
+    val appVersion      = "1.0-SNAPSHOT"
+
+    val appDependencies = Seq(
+    )
+
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+      scalaVersion := "2.10.3"
+    )
+
+}
