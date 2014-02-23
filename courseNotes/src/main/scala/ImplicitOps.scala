1a86d096f7eb36732a5d4a7226ade53b237cba5b
Mike Slinn <mslinn@micronauticsresearch.com>
2014-02-22 22:41:23 -0800

    -

--- courseNotes/src/main/scala/ImplicitOps.scala
+++ courseNotes/src/main/scala/ImplicitOps.scala
@@ -58,18 +58,3 @@ object AppleFanBoi extends App {
   println(s"I have an iPhone ${4.s}")
 }
 
-/** @see https://issues.scala-lang.org/browse/SI-4975?jql=text%20~%20%22implicit%20class%22 */
-object ImplicitOps3 extends App {
-  implicit val defaultMultiplier = Multiplier(2)
-
-  implicit val defaultDivider = Divider(3)
-
-  def multiply(value: Int)(implicit multiplier: Multiplier): Int = value * multiplier.value
-
-  def divide(value: Int)(implicit divider: Divider): Int = value / divider.value
-
-  //println(s"multiply(2)(3)=${multiply(2)(3)}")
-  println(s"multiply(5)=${multiply(5)}")
-  //println(s"divide(12)(4)=${divide(12)(4)}")
-  println(s"divide(9)=${divide(9)}")
-}
