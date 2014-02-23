94ffdcbe2b4580793b43bdb7038424e54030d3c7
Mike Slinn <mslinn@micronauticsresearch.com>
2014-02-22 23:15:06 -0800

    -

--- courseNotes/src/main/scala/ImplicitOps.scala
+++ courseNotes/src/main/scala/ImplicitOps.scala
@@ -36,6 +36,29 @@ object ImplicitOps2 extends App {
   println(s"divide(9)=${divide(9)}")
 }
 
+case class Multiplier2(value: Int) extends AnyVal
+
+case class Divider2(value: Int) extends AnyVal
+
+object ImplicitOps3 extends App {
+  implicit val defaultMultiplier = Multiplier2(2)
+
+  implicit val defaultDivider = Divider2(3)
+
+  def multiply(value: Int)(implicit multiplier: Multiplier2): Int = value * multiplier.value
+
+  def divide(value: Int)(implicit divider: Divider2): Int = value / divider.value
+
+  implicit def intToMultiplier(int: Int): Multiplier2 = Multiplier2(int)
+
+  implicit def intToDivider(int: Int): Divider2 = Divider2(int)
+
+  println(s"multiply(2)(3)=${multiply(2)(3)}")
+  println(s"multiply(5)=${multiply(5)}")
+  println(s"divide(12)(4)=${divide(12)(4)}")
+  println(s"divide(9)=${divide(9)}")
+}
+
 package yeller {
   case class Yeller(s: String) {
     def yell: String = s.toUpperCase + "!!"
@@ -58,3 +81,19 @@ object AppleFanBoi extends App {
   println(s"I have an iPhone ${4.s}")
 }
 
+package efficientYeller {
+  /** This value class does not require new object allocations */
+  case class Yeller(val s: String) extends AnyVal {
+    def yell: String = s.toUpperCase + "!!"
+  }
+
+  object `package` {
+    implicit def stringToYeller(s: String): Yeller = Yeller(s)
+  }
+}
+
+object EfficientYellerMain extends App {
+  import efficientYeller._
+
+  println("Look out".yell)
+}
