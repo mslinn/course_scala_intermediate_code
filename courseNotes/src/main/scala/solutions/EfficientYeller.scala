d165a42cca9bd8c2514537f824244d51a38f651b
Mike Slinn <mslinn@micronauticsresearch.com>
2014-03-02 14:03:57 -0800

    -

--- courseNotes/src/main/scala/solutions/EfficientYeller.scala
+++ courseNotes/src/main/scala/solutions/EfficientYeller.scala
@@ -1,14 +1,15 @@
 package solutions.yeller
 
-object EfficientYeller extends App {
-  println("Look out".yell)
-}
-
-/** This value class does not require new object allocations */
 case class Yeller(s: String) extends AnyVal {
   def yell: String = s.toUpperCase + "!!"
+  def whisper: String = "Shhh! " + s.toLowerCase
 }
 
 object `package` {
   implicit def stringToYeller(s: String): Yeller = Yeller(s)
+}
+
+object EfficientYeller extends App {
+  println("Look out".yell)
+  println("This is a secret".whisper)
 }
