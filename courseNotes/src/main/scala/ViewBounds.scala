ac1bed85d1896706a674b476ac494b764e71142e
Mike Slinn <mslinn@micronauticsresearch.com>
2014-03-03 20:10:32 -0800

    -

--- courseNotes/src/main/scala/ViewBounds.scala
+++ courseNotes/src/main/scala/ViewBounds.scala
@@ -1,24 +1,3 @@
-class X(val x: Int)
-
-class Y(val x: Int, val y: Int)
-
-class Z(val x: Int, val y: Int, val z: Int) {
-  def sum = x + y + z
-}
-
-object ViewBounds {
-  implicit def intToX(i: Int): X = new X(i)
-  implicit def xToY[XView <% X](x: XView): Y = new Y(x.x, 0)
-  implicit def yToZ[YView <% Y](y: YView): Z = new Z(y.x, y.y, 0)
-
-  println(5.sum)
-  println(new Y(2, 3).sum)
-  println(new Z(1, 2, 3).sum)
-
-  implicitly[Ordering[(Int, String)]].compare( (1, "b"), (1, "a") )
-}
-
-
 object TypesafeEquality extends App {
   class Dog3(val name: String) {
     override def equals(that: Any): Boolean = canEqual(that) && hashCode==that.hashCode
@@ -34,7 +13,7 @@ object TypesafeEquality extends App {
   val dog3b = new Dog3("Fido3b")
   val maybeDog3a = Some(dog3a)
   println(s"Comparing dog3 with itself: ${dog3a===dog3b}")
-  //\println(s"Comparing dog3 with maybeDog3: ${dog3a===maybeDog3a}")
+  //println(s"Comparing dog3 with maybeDog3: ${dog3a===maybeDog3a}")
 }
 
 object AllEquality extends App {
@@ -44,4 +23,25 @@ object AllEquality extends App {
 
   println(s"Comparing 3 with 4: ${3===4}")
   //println(s"Comparing 3 with Some(3): ${3===Some(3)}")
+}
+
+
+object ViewBounds {
+  class X(val x: Int)
+
+  class Y(val x: Int, val y: Int)
+
+  class Z(val x: Int, val y: Int, val z: Int) {
+    def sum = x + y + z
+  }
+
+  implicit def intToX(i: Int): X = new X(i)
+  implicit def xToY[XView <% X](x: XView): Y = new Y(x.x, 0)
+  implicit def yToZ[YView <% Y](y: YView): Z = new Z(y.x, y.y, 0)
+
+  println(5.sum)
+  println(new Y(2, 3).sum)
+  println(new Z(1, 2, 3).sum)
+
+  implicitly[Ordering[(Int, String)]].compare( (1, "b"), (1, "a") )
 }
