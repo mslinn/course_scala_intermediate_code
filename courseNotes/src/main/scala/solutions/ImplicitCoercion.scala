e92593df12cffa40833c2a54c63e0bd25315b650
Mike Slinn <mslinn@micronauticsresearch.com>
2014-03-03 17:26:42 -0800

    -

---
+++ courseNotes/src/main/scala/solutions/ImplicitCoercion.scala
@@ -0,0 +1,37 @@
+package solutions
+
+object ImplicitCoercion extends App {
+  case class Complex(re: Double, im: Double) {
+    def + (that: Complex) = new Complex(re + that.re, im + that.im)
+
+    def -(that: Complex) =  new Complex(this.re - that.re, this.im - that.im)
+
+    override def toString = s"$re + ${im}i"
+  }
+
+  implicit def doubleToComplex(d: Double) = Complex(d, 0)
+
+  implicit def tupleToComplex1(t: (Int, Int))       = Complex(t._1, t._2)
+  implicit def tupleToComplex2(t: (Int, Float))     = Complex(t._1, t._2)
+  implicit def tupleToComplex3(t: (Float, Int))     = Complex(t._1, t._2)
+  implicit def tupleToComplex4(t: (Float, Float))   = Complex(t._1, t._2)
+  implicit def tupleToComplex5(t: (Int, Double))    = Complex(t._1, t._2)
+  implicit def tupleToComplex6(t: (Double, Int))    = Complex(t._1, t._2)
+  implicit def tupleToComplex7(t: (Double, Double)) = Complex(t._1, t._2)
+  implicit def tupleToComplex8(t: (Float, Double))  = Complex(t._1, t._2)
+  implicit def tupleToComplex9(t: (Double, Float))  = Complex(t._1, t._2)
+
+  println(s"""Complex(2, 5) + 5.0 = ${Complex(2, 5) + 5.0}""")
+  println(s"""5.0 + Complex(1, -2) = ${5.0 + Complex(1, -2)}""")
+
+  println(s"""Complex(2.0f, 5) + 5.0 = ${Complex(2.0f, 5) + 5.0}""")
+  println(s"""Complex(2, 5.0f) + 5.0 = ${Complex(2, 5.0f) + 5.0}""")
+  println(s"""Complex(2.0f, 5.0f) + 5.0 = ${Complex(2.0f, 5.0f) + 5.0}""")
+
+  println(s"""Complex(2.0d, 5) + 5.0 = ${Complex(2.0d, 5) + 5.0}""")
+  println(s"""Complex(2, 5.0d) + 5.0 = ${Complex(2, 5.0d) + 5.0}""")
+
+  println(s"""Complex(2.0f, 5.0d) + 5.0 = ${Complex(2.0f, 5.0d) + 5.0}""")
+  println(s"""Complex(2.0d, 5.0f) + 5.0 = ${Complex(2.0d, 5.0f) + 5.0}""")
+  println(s"""Complex(2.0d, 5.0d) + 5.0 = ${Complex(2.0d, 5.0d) + 5.0}""")
+}
