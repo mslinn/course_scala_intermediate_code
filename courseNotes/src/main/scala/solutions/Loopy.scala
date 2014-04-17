543d11df8e82df46c995420f13bf93c2a51f03c6
Mike Slinn <mslinn@micronauticsresearch.com>
2014-04-17 11:12:09 -0700

    -

--- courseNotes/src/main/scala/solutions/Loopy.scala
+++ courseNotes/src/main/scala/solutions/Loopy.scala
@@ -1,16 +1,5 @@
 package solutions
 
-import java.util.Calendar
-
 object Loopy extends App {
-  val xmas = Calendar.getInstance()
-  xmas.set(Calendar.MONTH, Calendar.DECEMBER)
-  xmas.set(Calendar.DAY_OF_MONTH, 25)
-
-  def secsUntilXmas: Long = (xmas.getTimeInMillis - System.currentTimeMillis) / 1000
-
-  1 to 3 foreach { i =>
-    println(s"Only $secsUntilXmas seconds until Christmas!")
-    Thread.sleep(5000)
-  }
+  1 to 10 filter( _%2==0) foreach { i => println("abcdefghijk".substring(0, i)) }
 }
