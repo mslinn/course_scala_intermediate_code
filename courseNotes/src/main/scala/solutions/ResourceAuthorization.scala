18f5a0d55fba1d61ef539a11f59047581c3257ca
Mike Slinn <mslinn@micronauticsresearch.com>
2014-04-19 13:07:29 -0700

    -

--- courseNotes/src/main/scala/solutions/ResourceAuthorization.scala
+++ courseNotes/src/main/scala/solutions/ResourceAuthorization.scala
@@ -12,37 +12,43 @@ object ResourceAuthorization extends App {
   val resources      = mutable.HashMap.empty[Long, Resource]
   val resourceAccess = mutable.HashMap.empty[User, List[Resource]]
 
+  /** Add the given user to the users HashMap */
   def add(user: User) = {
     users.put(user.userId, user)
     user
   }
 
+  /** Add the given resource to the resources HashMap */
   def add(resource: Resource) = {
     resources.put(resource.id, resource)
     resource
   }
 
+  /** Use a for-comprehension to write this method
+    * @return Some User if the user with the given userId and password is returned by findUserById. */
   def authenticate(userId: String, password: String): Option[User] =
     for {
       user <- findUserById(userId) if user.password == password
     } yield user
 
+  /** Add the given resource to the list of resources assigned to the user in resourceAccess */
   def authorize(user: User, resource: Resource): Unit = {
     val newResources: List[Resource] = resource +: resourceAccess.get(user).getOrElse(Nil)
     resourceAccess.put(user, newResources)
   }
 
+  /** @return Some[Resource] if an entry for resourceId is found in resources */
   def findResourceById(resourceId: Long): Option[Resource] = resources.get(resourceId)
 
+  /** @return Some[User] if an entry for userId is found in users */
   def findUserById(userId: String): Option[User] = users.get(userId)
 
-  /** @return Option[authorizationToken] */
   def isUserAuthorized(userId: String, password: String, resourceId: Long): Option[AuthorizationToken] = {
-    val result = for {
+    val token = for {
       user <- authenticate(userId, password)
       resource <- findResourceById(resourceId)
     } yield AuthorizationToken(user, resource)
-    result
+    token
   }
 
   val fred   = add(User("Fred Flintstone", "Yabbadabbadoo", "fflintstone"))
@@ -68,11 +74,11 @@ object ResourceAuthorization extends App {
     val userName = findUserById(userId).map(_.name).getOrElse(s"User with id $userId")
     val msg = s"$userName and password $password ${ if (maybeAuthToken.isDefined) "can" else "can not" } access resource $resourceName"
     val msg2 = maybeAuthToken.map { msg + " until " + _.expires.toString }.getOrElse(msg)
-    println(msg2)
+    println(s"$msg2.")
   }
 
   report("fflintstone", "Yabbadabbadoo",  1)
   report("betty",       "Youtoo",         3)
   report("fflintstone", "ForgotPassword", 1)
-  report("nobody",      "nopass",         1)
+  report("nobody",      "nopass",         9)
 }
