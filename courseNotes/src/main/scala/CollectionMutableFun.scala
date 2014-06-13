080401980ea4cc3237c0f9c0f21a23a1cea52109
Mike Slinn <mslinn@micronauticsresearch.com>
2014-06-13 12:51:36 -0700

    Updated as per Aleksandar Prokopec

--- courseNotes/src/main/scala/CollectionMutableFun.scala
+++ courseNotes/src/main/scala/CollectionMutableFun.scala
@@ -1,33 +1,23 @@
-import collection._
+import scala.collection._
 
 object CollectionMutableFun extends App {
-  println(s"""List(1, 2, 3).toBuffer = ${List(1, 2, 3).toBuffer}""")
+  println( s"""List(1, 2, 3).toBuffer = ${List(1, 2, 3).toBuffer}""")
 
   val lb = mutable.ListBuffer(1, 2, 3)
-  println(s"""lb(0) * 3 = ${lb(0) * 3}""")
-  println(s"""lb.toArray = ${lb.toArray}""")
-  println(s"""lb.toSeq = ${lb.toSeq}""")
-  println(s"""lb.toIndexedSeq = ${lb.toIndexedSeq}""")
+  println( s"""lb(0) * 3 = ${lb(0) * 3}""")
+  println( s"""lb.toArray = ${lb.toArray}""")
+  println( s"""lb.toSeq = ${lb.toSeq}""")
+  println( s"""lb.toIndexedSeq = ${lb.toIndexedSeq}""")
 
   val mb = new mutable.ArrayBuffer[String] with mutable.SynchronizedBuffer[String]
 
   class SynchronizedArrayBuffer[T] extends mutable.ArrayBuffer[T] with mutable.SynchronizedBuffer[T]
+
   val mb2 = new SynchronizedArrayBuffer[String]
 
 
-  println(s"""mutable.WeakHashMap(1 -> "one", 33 -> "thirty-three", 4 -> "four") = ${mutable.WeakHashMap(1 -> "one", 33 -> "thirty-three", 4 -> "four")}""")
+  println( s"""mutable.WeakHashMap(1 -> "one", 33 -> "thirty-three", 4 -> "four") = ${mutable.WeakHashMap(1 -> "one", 33 -> "thirty-three", 4 -> "four")}""")
 
-  import java.util.concurrent.ConcurrentHashMap
-  import collection.JavaConverters._
-  val cmap = new ConcurrentHashMap[String, String].asScala
-
-  import collection.concurrent.TrieMap
-  val tm = TrieMap.empty[String, String].withDefaultValue("default")
-  tm.put("key1", "value1")
-  tm.put("key2", "value2")
-  println(s"""tm.get("key1") = ${tm.get("key1")}""")
-  println(s"""tm("key1") = ${tm("key1")}""")
-  println(s"""tm("x") = ${tm("x")}""")
 
   val queue = mutable.Queue.empty[String]
   queue += "asdf"
@@ -39,6 +29,46 @@ object CollectionMutableFun extends App {
   println(s"""mutable.LinkedHashSet(1) = ${mutable.LinkedHashSet(1)}""")
   println(s"""mutable.LinkedHashMap(1 -> "one") = ${mutable.LinkedHashMap(1 -> "one")}""")
   println(s"""mutable.DoubleLinkedList(1 -> "two") = ${mutable.DoubleLinkedList(1 -> "two")}""")
+}
+
+object CMap extends App {
+  import collection.concurrent.{TrieMap, Map => CMap }
+  import collection.JavaConverters._
+  import java.util.concurrent.{ConcurrentSkipListMap, ConcurrentHashMap => JConcurrentHashMap }
+
+  val cmap = new JConcurrentHashMap[String, String].asScala
+  println(s"cmap = $cmap")
+
+  val cslm = new ConcurrentSkipListMap[String, String].asScala
+  println(s"""cslm = $cslm""")
+
+  val cslm2 = new ConcurrentSkipListMap[String, String].asScala.withDefaultValue("default")
+  println(s"""cslm2("") = $cslm2("")""")
+
+
+  val tm = TrieMap.empty[String, String].withDefaultValue("default")
+  tm.put("key1", "value1")
+  tm.put("key2", "value2")
+  println(s"""tm.get("key1") = ${tm.get("key1")}""")
+  println(s"""tm("key1") = ${tm("key1")}""")
+  println(s"""tm("x") = ${tm("x")}""")
+
+
+  def ensureKeyIsPresent[U, V](map: CMap[U, V], key: U, value: V): CMap[U, V] = {
+    map.putIfAbsent(key, value)
+    map
+  }
+
+  val cmap2 = ensureKeyIsPresent(new JConcurrentHashMap[String, String].asScala, "key", "value"))
+
+  val cmap3 = ensureKeyIsPresent(new TrieMap, "key", "value")
+  println(s"cmap3 = $cmap3")
+
+  val cmap4 = ensureKeyIsPresent(new ConcurrentSkipListMap().asScala, "key", "value")
+  println(s"cmap4 = $cmap4")
+
+  val cmap5 = ensureKeyIsPresent(new JConcurrentHashMap().asScala, "key", "value")
+  println(s"cmap5 = $cmap5")
 }
 
 object Memo extends App with Memoize {
