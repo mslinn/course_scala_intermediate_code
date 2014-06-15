8ec12253ab3f76aa75cb62e996541199d842f683
Mike Slinn <mslinn@micronauticsresearch.com>
2014-06-15 13:02:33 -0700

    -

--- courseNotes/src/main/scala/solutions/ConfigFlatNest.scala
+++ courseNotes/src/main/scala/solutions/ConfigFlatNest.scala
@@ -15,7 +15,24 @@ object ConfigFlatNest extends App {
   val tuples = flatKeyValueTuples(configApp).sortBy(_._1)
   val result1 = tuples.map( x => s"${x._1} = ${x._2}").mkString("", "\n", "\n")
 
-  val writer = new PrintWriter(new File(System.getProperty("user.home"), "application.conf"))
+  val file = new File(System.getProperty("user.home"), "application.conf")
+  val writer = new PrintWriter(file)
   writer.write(result1)
   writer.close()
+
+  val originalEntrySet = configApp.entrySet
+  val x = configApp.getAnyRef("akka.io.tcp.finish-connect-retries")
+  val confFlat = ConfigFactory.parseFile(file)
+  val matched = confFlat.entrySet.asScala.forall { kv =>
+    val flatKey = kv.getKey
+    val flatValue = kv.getValue.render
+    val originalValue = configApp.getAnyRef(flatKey)
+    if (originalValue != flatValue)
+      println(s"$flatKey: $originalValue (${originalValue.getClass.getName}) != $flatValue  (${flatValue.getClass.getName})")
+    originalValue == flatValue
+  }
+  if (matched)
+    println(s"Flat version in ${file.getAbsolutePath} matches original hierarchical version")
+  else
+    println(s"Flat version in ${file.getAbsolutePath} does not match original hierarchical version")
 }
