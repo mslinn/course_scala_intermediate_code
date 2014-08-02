16e6f4d2aaf40657bb91a903b0e7d7530cbefbd6
Mike Slinn <mslinn@micronauticsresearch.com>
2014-08-02 08:11:46 -0700

    -

--- courseNotes/src/main/scala/Structural.scala
+++ courseNotes/src/main/scala/Structural.scala
@@ -15,14 +15,42 @@ object Ducks1 extends App {
 }
 
 object Ducks2 extends App {
-  type Duck = { def quack(count: Int): Unit }
+  type Duck = {
+    def quack(count: Int): Unit
+  }
 
   case class Mallard(age: Double) {
-    def quack(count: Int): Unit = println("Quack! " * count)
+    def quack(count: Int): Unit = println("Mallard: " + "Quack! " * count)
   }
 
   case class FrenchDuck(weight: Double) {
-    def quack(count: Int): Unit = println("Le quack! " * count)
+    def quack(count: Int): Unit = println("FrenchDuck: " + "Le quack! " * count)
+  }
+
+  val mallard: Duck = Mallard(4)
+  val frenchDuck: Duck = FrenchDuck(5)
+
+  mallard.quack(2)
+  frenchDuck.quack(3)
+}
+
+object Ducks3 extends App {
+  type Duck = {
+    def quack(count: Int): Unit
+
+    val color: String
+  }
+
+  case class Mallard(age: Double) {
+    def quack(count: Int): Unit = println(s"Mallard  has $color feathers: " + "Quack! " * count)
+
+    val color = "yellow"
+  }
+
+  case class FrenchDuck(weight: Double) {
+    def quack(count: Int): Unit = println(s"FrenchDuck has $color feathers: " + "Le quack! " * count)
+
+    val color = "blue"
   }
 
   val mallard: Duck = Mallard(4)
@@ -37,27 +65,27 @@ object GoodDuck extends App {
     def quack(count: Int): Unit
   }
 
-  class Duck1 extends DuckLike {
-    def quack(count: Int) = println("Duck1: " + "Quack! " * count)
+  class DuckExtendsTrait extends DuckLike {
+    def quack(count: Int) = println("DuckExtendsTrait: " + "Quack! " * count)
 
-    def waddle() = println("Duck1 Waddling...")
+    def waddle() = println("DuckExtendsTrait Waddling...")
   }
 
   abstract class AbstractDuck {
     def quack(count: Int): Unit
   }
 
-  class Duck2 extends AbstractDuck {
-    def quack(count: Int) = println("Duck2: " + "Quack! " * count)
+  class DuckConcrete extends AbstractDuck {
+    def quack(count: Int) = println("DuckConcrete: " + "Quack! " * count)
 
-    def waddle() = println("Duck2: Waddling...")
+    def waddle() = println("DuckConcrete: Waddling...")
   }
 
-  val duck1 = new Duck1
+  val duck1 = new DuckExtendsTrait
   duck1.quack(3)
   duck1.waddle()
 
-  val duck2 = new Duck2
+  val duck2 = new DuckConcrete
   duck2.quack(3)
   duck2.waddle()
 }
