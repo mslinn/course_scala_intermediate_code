bdaf639571cf6a33897779fbee19fcbe03277fc1
Mike Slinn <mslinn@mslinn.com>
2014-08-24 11:03:58 -0700

    -

--- courseNotes/src/main/scala/strInterp.scala
+++ courseNotes/src/main/scala/strInterp.scala
@@ -1,8 +1,10 @@
-implicit class MapLookup(val sc: StringContext) {
-  val map = Map(("a", 1), ("b", 2), ("c", 3)).withDefaultValue(0)
-   
-  def $(args: Any*): Int = {
-    val orig = sc.s(args: _*)
-    map.get(orig)
+object StrInterp {
+  implicit class MapLookup(val sc: StringContext) {
+    val map = Map(("a", 1), ("b", 2), ("c", 3)).withDefaultValue(0)
+
+    def $(args: Any*): Int = {
+      val orig = sc.s(args: _*)
+      map(orig)
+    }
   }
 }
