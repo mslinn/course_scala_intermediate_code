0b4677f1d96408599a981ad735b38f485391a387
Mike Slinn <mslinn@micronauticsresearch.com>
2014-08-29 04:20:48 -0700

    -

--- courseNotes/src/main/scala/ClassInitProblem.scala
+++ courseNotes/src/main/scala/ClassInitProblem.scala
@@ -1,5 +1,25 @@
-object Outer { 
+// This hangs:
+object Outer extends App { 
   val x = 3
   List(0).par.map(_ + Outer.x) 
 }
 
+object NoHanging1 extends App {
+  class Outer {
+    val x = 3
+    List(0).par.map(_ + Outer.this.x) 
+  }
+
+  new Outer().x
+}
+
+object NoHanging2 extends App {
+  def method(x: Int) = List(0).par.map(_ + x)
+
+  object Outer { 
+    val x = 3
+    method(x)
+  }
+
+  val y = Outer.x
+}
