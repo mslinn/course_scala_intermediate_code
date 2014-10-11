d084f085938a69e1a157c17bf35fd35504640110
Mike Slinn <mslinn@micronauticsresearch.com>
2014-10-11 03:21:18 +0100

    -

--- courseNotes/src/main/scala/FutureRace.scala
+++ courseNotes/src/main/scala/FutureRace.scala
@@ -4,16 +4,25 @@ object FutureRace extends App {
   import scala.util.{Success, Failure}
   import scala.concurrent.duration.Duration
 
-  val pool: ExecutorService = Executors.newFixedThreadPool(1)
+  val threadCount: Int = try {
+    if (args.length==1) args(0).toInt else 1
+  } catch {
+    case e: Exception =>
+      println("You can specify the number of threads in the threadpool on the command line; default is 1")
+      System.exit(-1)
+      1
+  }
+
+  val pool: ExecutorService = Executors.newFixedThreadPool(threadCount)
   implicit val ec = ExecutionContext.fromExecutor(pool)
 
-  @volatile var offset = 6 // @volatile does not solve race conditions
+  @volatile var offset = 6 // @volatile does not affect race conditions
   def accessor = offset
 
   val f1 = Future {
     2 + 3 + offset // will be executed asynchronously
   } andThen {
-    case Success(result)   => println("Race Scala result1 : " + result)
+    case Success(result)   => println("Race Scala result 1 : " + result)
     case Failure(exception) => println("Race Scala exception 1: " + exception.getMessage)
   }
 
@@ -27,4 +36,5 @@ object FutureRace extends App {
   println("End of mainline, offset = " + offset)
 
   Await.ready(Future.sequence(List(f1, f2)), Duration.Inf)
+  pool.shutdown()
 }
