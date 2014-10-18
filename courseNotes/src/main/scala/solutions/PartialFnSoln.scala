3f7774a217e6327505b3f286fda0fb9bf5e2faf7
Mike Slinn <mslinn@micronauticsresearch.com>
2014-10-18 11:53:53 -0700

    -

---
+++ courseNotes/src/main/scala/solutions/PartialFnSoln.scala
@@ -0,0 +1,13 @@
+package solutions
+
+object PartialFnSoln extends App {
+  val squareRoot: PartialFunction[Double, Double] = {
+     case d if d >= 0 => math.sqrt(d)
+  }
+
+  println(s"squareRoot.isDefinedAt(-1)=${squareRoot.isDefinedAt(-1)}")
+  println(s"squareRoot(3)=${squareRoot(3)}")
+
+  val sqrtList1 = List(0.5, -0.2, 4).collect(squareRoot)
+  val sqrtList2 = List(0.5, -0.2, 4) collect squareRoot
+}
