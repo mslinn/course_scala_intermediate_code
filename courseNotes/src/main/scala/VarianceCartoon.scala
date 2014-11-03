19dbc175f0d7364a98731e70316dcfc33c0fa181
Mike Slinn <mslinn@micronauticsresearch.com>
2014-11-03 03:07:56 +0000

    Using traits instead of classes

--- courseNotes/src/main/scala/VarianceCartoon.scala
+++ courseNotes/src/main/scala/VarianceCartoon.scala
@@ -1,31 +1,31 @@
-object VarianceCartoon extends App{
-  class EnergySource
-  class Vegetable extends EnergySource
-  class Bamboo extends Vegetable
+object VarianceCartoon extends App {
+  trait EnergySource
+  trait Vegetable extends EnergySource
+  trait Bamboo extends Vegetable
 
-  class Consumer
-  class Vegetarian extends Consumer
-  class Panda extends Vegetarian
+  trait Consumer
+  trait Vegetarian extends Consumer
+  trait Panda extends Vegetarian
 
-  class Entertainment
-  class Music extends Entertainment
-  class Metal extends Music
+  trait Entertainment
+  trait Music extends Entertainment
+  trait Metal extends Music
 
-  class Producer
-  class Musician extends Producer
-  class HeadBanger extends Musician
+  trait Producer
+  trait Musician extends Producer
+  trait HeadBanger extends Musician
 
-  class Entertainer[???] {
+  trait Entertainer[???] {
     def feed(entertainer: ???): Unit = {}
     def entertain(entertainer: ???): Unit = {}
   }
 
-  class Restaurant {
+  trait Restaurant {
     val producers: Seq[Producer] = List.empty
     val consumers: Seq[Consumer] = List.empty
 
-    // define a method that can feed any Consumer the appropriate EnergySource or subclass
+    // define a method that can feed any Consumer the appropriate EnergySource or subtrait
 
-    // define a method that can employ any Producer or subclass to provide the desired entertainment
+    // define a method that can employ any Producer or subtrait to provide the desired entertainment
   }
 }
