3de26d11cacd71b0b0e79379d8b2ed2d132f9fc8
Mike Slinn <mslinn@micronauticsresearch.com>
2015-01-12 17:00:34 -0800

    Ready to update video

--- courseNotes/src/main/scala/collections/CollectionSorting.scala
+++ courseNotes/src/main/scala/collections/CollectionSorting.scala
@@ -1,18 +1,18 @@
 package collections
 
-object CollectionSorting extends App {
-  println(s"""List(3, 7, 5, 2).sortWith((x, y) => x < y) = ${List(3, 7, 5, 2).sortWith((x, y) => x < y)}""")
-  println(s"""List(3, 7, 5, 2).sortWith(_ < _) = ${List(3, 7, 5, 2).sortWith(_ < _)}""")
+object CollectionOrdered extends App {
+  println( s"""List(3, 7, 5, 2).sortWith((x, y) => x < y) = ${List(3, 7, 5, 2).sortWith((x, y) => x < y)}""")
+  println( s"""List(3, 7, 5, 2).sortWith(_ < _) = ${List(3, 7, 5, 2).sortWith(_ < _)}""")
 
   class Thing(val i: Int, val s: String) extends Ordered[Thing] {
-    def compare(that: Thing) =  {
+    def compare(that: Thing) = {
       val primaryKey = this.i - that.i
-      if (primaryKey!=0) primaryKey else this.s compare that.s
+      if (primaryKey != 0) primaryKey else this.s compare that.s
     }
 
     override def equals(other: Any) = {
       val that = other.asInstanceOf[Thing]
-      this.i == that.i && this.s==that.s
+      this.i == that.i && this.s == that.s
     }
 
     override def hashCode = super.hashCode
@@ -24,49 +24,64 @@ object CollectionSorting extends App {
   val thing2 = new Thing(2, "y")
   val thing3 = new Thing(3, "x")
 
-  println(s"""thing1>thing2 = ${thing1>thing2}""")
-  println(s"""thing1<thing2 = ${thing1<thing2}""")
-  println(s"""thing1 == Thing(1, "z") = ${thing1 == new Thing(1, "z")}""")
-  println(s"""thing1<=thing2 = ${thing1<=thing2}""")
-  println(s"""thing1>=thing2 = ${thing1>=thing2}""")
+  println( s"""thing1>thing2 = ${thing1 > thing2}""")
+  println( s"""thing1<thing2 = ${thing1 < thing2}""")
+  println( s"""thing1 == Thing(1, "z") = ${thing1 == new Thing(1, "z")}""")
+  println( s"""thing1<=thing2 = ${thing1 <= thing2}""")
+  println( s"""thing1>=thing2 = ${thing1 >= thing2}""")
 
   val things = Array(thing2, thing1, thing3)
-  println(s"""things.sorted = ${things.sorted.mkString(", ")}""")
-  println(s"""things before quicksort = ${things.mkString(", ")}""")
+  println( s"""things.sorted = ${things.sorted.mkString(", ")}""")
+  println( s"""things before quicksort = ${things.mkString(", ")}""")
   util.Sorting.quickSort(things) // modifies things Array
-  println(s"""things after quicksort = ${things.mkString(", ")}""")
-  println(s"""things.sortWith((x, y) => x.i > y.i)=${things.sortWith((x, y) => x.i > y.i).mkString(", ")}""")
-  println(s"""things.sortWith((x, y) => x.s > y.s)=${things.sortWith((x, y) => x.s > y.s).mkString(", ")}""")
+  println( s"""things after quicksort = ${things.mkString(", ")}""")
+  println( s"""things.sortWith((x, y) => x.i > y.i)=${things.sortWith((x, y) => x.i > y.i).mkString(", ")}""")
+  println( s"""things.sortWith((x, y) => x.s > y.s)=${things.sortWith((x, y) => x.s > y.s).mkString(", ")}""")
+}
 
-  class Thang(val i: Int, val s: String) extends Ordering[Thang] {
-    def compare(a: Thang, b: Thang) =  {
-      val primaryKey = a.i - b.i
-      if (primaryKey!=0) primaryKey else a.s compare b.s
+object CollectionOrdering extends App {
+  class Thang(val i: Int, val s: String) extends Ordered[Thang] {
+    def compare(that: Thang): Int =  {
+      val primaryKey = this.i - that.i
+      if (primaryKey!=0) primaryKey else this.s compare that.s
     }
 
-    override def equals(other: Any) = {
+    override def equals(other: Any): Boolean = {
       val that = other.asInstanceOf[Thang]
       this.i == that.i && this.s==that.s
     }
 
-    override def hashCode = super.hashCode
+    override def hashCode: Int = super.hashCode
 
     override def toString = s"Thang($i, $s)"
   }
 
-  object Thang {
-    implicit val ThangOrdering = Ordering.by { thang: Thang =>
-      (thang.i, thang.s)
-    }
-  }
-
-  val orderByI = Ordering.by { thang: Thang => thang.i }
-  val orderByS = Ordering.by { thang: Thang => thang.s }
-  val thangs = Array(new Thang(1, "x"), new Thang (33, "b"), new Thang(4, "m"))
+  val thangs = Array(new Thang(1, "x"), new Thang(1, "y"), new Thang (33, "b"), new Thang(4, "m"), new Thang(4, "n"))
 
   println(s"""thangs = ${thangs.mkString(", ")}""")
+  println(s"""thangs.sorted = ${thangs.sorted.mkString(", ")}""")
+  println(s"""thangs.sorted(Ordering[Thang]) = ${thangs.sorted(Ordering[Thang]).mkString(", ")}""")
+
+  val orderByI: Ordering[Thang] = Ordering.by { _.i }
   println(s"""thangs.sorted(orderByI) = ${thangs.sorted(orderByI).mkString(", ")}""")
+
+  val orderByIReverse: Ordering[Thang] = orderByI.reverse
+  println(s"""thangs.sorted(orderByIReverse) = ${thangs.sorted(orderByIReverse).mkString(", ")}""")
+
+  val orderByS: Ordering[Thang] = Ordering.by { _.s }
   println(s"""thangs.sorted(orderByS) = ${thangs.sorted(orderByS).mkString(", ")}""")
+
+  val orderBySReverse: Ordering[Thang] = orderByS.reverse
+  println(s"""thangs.sorted(orderBySReverse) = ${thangs.sorted(orderBySReverse).mkString(", ")}""")
+
+  val orderBySandI: Ordering[Thang] = Ordering.by { x => (x.s, x.i) }
+  println(s"""thangs.sorted(orderBySandI) = ${thangs.sorted(orderBySandI).mkString(", ")}""")
+
+  val orderByIandS: Ordering[Thang] = Ordering.by { x => (x.i, x.s) }
+  println(s"""thangs.sorted(orderByIandS) = ${thangs.sorted(orderByIandS).mkString(", ")}""")
+
+  println(s"""thangs.sorted(orderBySandI.reverse) = ${thangs.sorted(orderBySandI.reverse).mkString(", ")}""")
+  println(s"""thangs.sorted(orderByIandS.reverse) = ${thangs.sorted(orderByIandS.reverse).mkString(", ")}""")
 
   val ordering = implicitly[Ordering[(Int, String)]]
   println(s"""ordering.compare( (1, "b"), (1, "a") ) = ${ordering.compare( (1, "b"), (1, "a") )}""")
@@ -75,6 +90,8 @@ object CollectionSorting extends App {
 }
 
 object PersonSorting extends App {
+  import scala.language.implicitConversions
+
   case class Person(name: String)
 
   val array = Array(Person("Chloe"), Person("Andrea"), Person("BeeKay"))
@@ -83,7 +100,7 @@ object PersonSorting extends App {
     def compare(that: Person) = person.name.compare(that.name)
   }
 
-  implicit def personToOrdered(p: Person) = new OrderedPerson(p)
+  implicit def personToOrdered(p: Person): OrderedPerson = new OrderedPerson(p)
 
   util.Sorting.quickSort(array)
 }
