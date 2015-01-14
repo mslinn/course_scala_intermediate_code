639ddf72436205bab768b0b9b0b17ce85dcb5a9c
Mike Slinn <mslinn@micronauticsresearch.com>
2015-01-14 07:14:03 -0800

    Added new Partial Function excercise

--- courseNotes/src/main/scala/solutions/Trying.scala
+++ courseNotes/src/main/scala/solutions/Trying.scala
@@ -9,7 +9,7 @@ object Trying extends App {
   /** @return collection of all successful values from the given collection of Try */
   def successes[A](tries: Seq[Try[A]]): Seq[A] = tries.collect { case Success(t) => t }
 
-  /** @return a Try of a collection from a collection of Try. If many Exceptions are encounted, only one Exception needs to be captured. */
+  /** @return a Try of a collection from a collection of Try. If many Exceptions are encountered, only one Exception needs to be captured. */
   def sequence[A](tries: Seq[Try[A]]): Try[Seq[A]] = Try(tries.map(_.get))
 
   /** @return a Tuple2 containing a collection of all failures and all the successes */
