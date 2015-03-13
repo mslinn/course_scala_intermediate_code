7524ff5f6b7d78952dff2a1b9fbd90362379636e
Mike Slinn <mslinn@micronauticsresearch.com>
2015-03-12 17:41:41 -0700

    Added sample code for Pattern Matching on Sequences lecture

--- courseNotes/src/main/scala/solutions/ExtractFileType.scala
+++ courseNotes/src/main/scala/solutions/ExtractFileType.scala
@@ -2,12 +2,14 @@ package solutions
 
 object ExtractFileType extends App {
   def findExtension(s: String): Option[String] = s.reverse.split("\\.") match {
-    case Array(ext, _*) if ext.nonEmpty => Some(ext.reverse)
+    case Array(ext, _*) if ext.nonEmpty && !s.endsWith(".") => Some(ext.reverse)
     case _ => None
   }
 
+  println(s"""findExtension(".")=${findExtension(".")}""")
   println(s"""findExtension("html")=${findExtension("html")}""")
   println(s"""findExtension(".html")=${findExtension(".html")}""")
   println(s"""findExtension("page.html")=${findExtension("page.html")}""")
+  println(s"""findExtension("page.")=${findExtension("page.")}""")
   println(s"""findExtension(""path/prefix.page.html")=${findExtension("path/prefix.page.html")}""")
 }
