27e0aa754a064e7657ec82e6799e3e07391f4422
Mike Slinn <mslinn@micronauticsresearch.com>
2019-09-08 10:41:22 -0700

    -

--- courseNotes/src/main/scala/AutoCloseableLike.scala
+++ courseNotes/src/main/scala/AutoCloseableLike.scala
@@ -1,10 +1,28 @@
 import java.io.{BufferedReader, File, FileReader}
 
 trait AutoCloseableLike {
-  def using[A <: AutoCloseable, B](resource: A)
-                                  (block: A => B): B =
-    try block(resource) finally resource.close()
+  def readLines(fileName: String): List[String] = readLines(new File(fileName))
 
-  def readLines(file: File): String =
-    using(new BufferedReader(new FileReader(file))) { _.readLine() }
+  def readLines(file: File): List[String] =
+    using(new BufferedReader(new FileReader(file)))
+       { reader => unfold(())(_ => Option(reader.readLine).map(_ -> ((): Unit))) }
+
+  def read(file: File): String = readLines(file).mkString
+
+  def read(fileName: String): String = read(new File(fileName))
+
+  def using[A <: AutoCloseable, B]
+           (resource: A)
+           (f: A => B): B =
+    try f(resource) finally resource.close()
+
+  /** Scala 2.13 defines `Iterator.unfold`. This is essentially the same method, provided for all Scala versions. */
+  def unfold[A, S](start: S)
+                  (op: S => Option[(A, S)]): List[A] =
+    Iterator.
+      iterate(op(start))(_.flatMap{ case (_, s) => op(s) })
+        .map(_.map(_._1))
+        .takeWhile(_.isDefined)
+        .flatten
+        .toList
 }
